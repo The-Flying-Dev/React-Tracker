f359e864a08bcf319364e0dc3f83726445345a45
The-Flying-Dev <leandranm@outlook.com>
2022-07-06 02:40:34 -0700

    Pass firts props to Card component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Cards, Chart, CountryPicker } from './components'; //imports directly from components/index.js
 import styles from './App.module.css';
 import { fetchData } from './api';
 
 class App extends React.Component {
 
-  async componentDidMount() {
-    const data = await fetchData();
+  //state declaration with empty object inside class
+  state = {
+    data: {},
+  }
 
-    console.log(data);
+  //first lifecycle method
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    //where the current State is being updated 
+    this.setState({ data: fetchedData });
   }
   
   render() {
+    //destructure the data object then pass it as a prop to the Card component
+    const { data } = this.state;
+
     return (                      
       <div className={styles.container}> {/* dynamic styles */}
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
